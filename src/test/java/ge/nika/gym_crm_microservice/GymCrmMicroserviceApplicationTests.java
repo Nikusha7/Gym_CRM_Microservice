8c32459ff08761f05f3700b6654a0c3cf6cb1ef2
Nikusha <nikushajv@gmail.com>
2024-11-22 15:57:58 +0400

    initial commit

---
+++ src/test/java/ge/nika/gym_crm_microservice/GymCrmMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.nika.gym_crm_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymCrmMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
